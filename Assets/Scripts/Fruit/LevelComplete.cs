d89441e95cf7564aac1ecaa8160cb8660623d654
Abel Pujol <abelpujol99@gmail.com>
2021-02-25 10:13:13 +0100

    All Works
    Lot of problems

---
+++ Assets/Scripts/Fruit/LevelComplete.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelComplete : MonoBehaviour
+{
+
+    public Text levelComplete;
+    public void Update()
+    {
+        allFruitsCollected();       
+    }
+    public void allFruitsCollected()
+    {
+        if (transform.childCount == 0)
+        {
+            levelComplete.gameObject.SetActive(true);
+            Invoke("ChangeScene", 2);
+        }
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
